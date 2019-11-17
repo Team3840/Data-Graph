40902aac7260d28fc886b0efef0b8ed8da3a470d
Todd Kruse <tkruse@deltamodtech.com>
2019-11-17 13:11:23 -0600

    Add File #1

---
+++ modStarter01.vb
@@ -0,0 +1,19 @@
+﻿Imports Microsoft.VisualBasic
+
+Public Class modStarter
+
+#Region "Variables"
+    Public mstrFileName As String
+
+#End Region
+
+#Region "Public Sub"
+
+    Public Sub DoEveryThing()
+        mstrFileName = "I Know"
+
+        messagebox.show(mstrFileName)
+    End Sub
+
+#End Region
+End Class
